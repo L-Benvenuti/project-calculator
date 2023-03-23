d20db67acbe5d87aa63a2d8affc03532d81fdf81
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-03-23 16:18:57 -0500

    add/change: classes + function to display text

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function add(a, b) { return a + b };
 function subtract(a, b) { return a - b };
 function multiply(a, b) { return a * b };
 function divide(a, b) { return a / b };
+
+// Text of display
+const display = document.querySelector('.display');
+let btns = document.querySelectorAll('.btn');
+btns.forEach(btn => btn.addEventListener('click', (e) => {
+    let choice = e.target.id
+    display.textContent = choice
+}));
